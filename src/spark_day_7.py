1b5cb75eb9f7e1604f4203fe6e4bb697515a9954
mk <manikandanvkm1@gmail.com>
2025-12-04 21:15:11 +0530

    updated day 7 solution

--- src/spark_day_7.py
+++ src/spark_day_7.py
@@ -30,20 +30,41 @@ df2 = df.groupBy("patient_id").agg(F.sum(F.when(F.col("temperature") > 100.4, 1)
 w = Window.partitionBy("patient_id").orderBy(F.col("reading_date"))
 df_q3 = df.withColumn("prev_reading_date", F.lag("reading_date").over(w))
 df_avg = df_q3.withColumn("days_gap", F.datediff("reading_date", "prev_reading_date"))
-df3 = df_avg.groupBy("patient_id").agg(F.avg("days_gap").alias("avg_days_gap"))
+df3 = df_avg.groupBy("patient_id").agg(F.avg("days_gap").alias("avg_days_gap")).drop("reading_date")
 
 #4️⃣ Find the Highest Pulse Reading AND the Date It Occurred
 w1 = Window.partitionBy("patient_id").orderBy(F.col("pulse").desc())
-#max_pulse = df.withColumn("max_pulse", F.max("pulse"))
+
 df4 = df.withColumn("rn", F.row_number().over(w1)) \
  .filter("rn=1") \
- .drop("rn").select("patient_id", "reading_date", "pulse")
+ .drop("rn").select("patient_id", F.col("pulse").alias("max_pulse"))
 
 
 #5️⃣ Label Overall Patient Risk Level
 w2 = Window.partitionBy("patient_id").orderBy(F.col("reading_date").desc())
 
-find_recent = ((df.withColumn("rn", F.row_number().over(w2))
+df5 = ((df.withColumn("rn", F.row_number().over(w2))
                .filter("rn= 1").withColumn("risk_level", F.when(F.col("temperature") > 100.4, "High Risk")
-               .when(F.col("oxygen") < 95, "Medium Risk").otherwise("Normal"))).orderBy(F.col("reading_date").desc())).drop("rn").select("patient_id", "reading_date", "risk_level")
-find_recent.show()
+               .when(F.col("oxygen") < 95, "Medium Risk").otherwise("Normal"))).orderBy(F.col("reading_date").desc())).drop("rn").select("patient_id", "risk_level")
+
+#6️⃣ Detect Missing Daily Records
+df = df.withColumn("reading_date", F.to_date("reading_date", "yyyy-MM-dd"))
+range_6_df = df.groupBy("patient_id").agg(F.min("reading_date").alias("first_reading_date"),F.max("reading_date").alias("latest_reading_date"))
+
+full_dates_6_df = range_6_df.withColumn("full_dates", F.sequence("first_reading_date", "latest_reading_date", F.expr("interval 1 day"))) \
+ .select("patient_id", F.explode("full_dates").alias("reading_date"))
+
+#Identify missing dates
+df6 = full_dates_6_df.join(df,["patient_id", "reading_date"],"left_anti")
+
+#7️⃣ Final Combined Output
+
+final_df = (
+ df1.alias("a").join(df2.alias("b"), "patient_id", "left").join(df3.alias("c"), "patient_id", "left")
+ .join(df4.alias("d"), "patient_id", "left").join(df5.alias("e"), "patient_id", "left")
+)
+
+final_df.show()
+
+
+
